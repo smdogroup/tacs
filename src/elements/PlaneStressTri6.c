6fe3fb3a3a7041ad3dec5b562eac4cf4d330d738
gkennedy <devnull@localhost>
2016-03-08 16:13:12 -0500

    added skeleton of PlaneStressTri6 element

---
+++ src/elements/PlaneStressTri6.c
@@ -0,0 +1,11 @@
+#include "PlaneStressTri6.h"
+
+PlaneStressTri6::PlaneStressTri6( PlaneStressStiffness * _stiff, 
+				  PlaneStressElementType type,
+				  int _componentNum ){
+
+}
+
+PlaneStressTri6::~PlaneStressTri6(){
+
+}
---
+++ src/elements/PlaneStressTri6.h
@@ -0,0 +1,45 @@
+#ifndef TACS_PLANE_STRESS_TRI6_H
+#define TACS_PLANE_STRESS_TRI6_H
+
+class PlaneStressTri6 : public TACS2DElement<6> {
+ public:
+  enum PlaneStressElementType { LINEAR, NONLINEAR };
+
+  PlaneStressTri6( PlaneStressStiffness * _stiff, 
+		   PlaneStressElementType type = LINEAR, 
+		   int _componentNum = 0 );
+  ~PlaneStressTri6();
+  
+    // Return the name of this element
+  // -------------------------------
+  const char * elementName() const { return elemName; }
+
+  // Retrieve the shape functions
+  // ----------------------------
+  void getShapeFunctions( const double pt[], double N[] );
+  void getShapeFunctions( const double pt[], double N[],
+			  double Na[], double Nb[] );
+
+  // Retrieve the Gauss points/weights
+  // ---------------------------------
+  int getNumGaussPts();
+  TacsScalar getGaussWtsPts( const int num, double pt[] ); 
+
+  // Functions for post-processing
+  // -----------------------------
+  void addOutputCount( int * nelems, int * nnodes, int * ncsr );
+  void getOutputData( unsigned int out_type, 
+		      double * data, int ld_data, 
+		      const TacsScalar Xpts[],
+		      const TacsScalar vars[] );
+  void getOutputConnectivity( int * con, int node );
+
+ private:
+  static const int NUM_NODES = 6;
+
+  int numGauss;
+  const double *gaussWts;
+  const double *gaussPts1, *gaussPts2;
+};
+
+#endif // TACS_PLANE_STRESS_TRI6_H
