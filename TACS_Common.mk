c47284cd66bb42e6162cb05b5e4695b5a390549c
gkennedy <devnull@localhost>
2017-01-26 14:56:50 -0500

    modified the Makefile in order to enable static/dynamic compilation

--- TACS_Common.mk
+++ TACS_Common.mk
@@ -16,7 +16,7 @@ TACS_DEBUG_CC_FLAGS = ${TACS_DEF} ${EXTRA_DEBUG_CC_FLAGS} ${METIS_INCLUDE} ${AMD
 TACS_CC_FLAGS = ${TACS_OPT_CC_FLAGS}
 
 # Set the linking flags to use
-TACS_LD_FLAGS = ${EXTRA_LD_FLAGS} -L${TACS_DIR}/lib/ -Wl,-rpath,${TACS_DIR}/lib -ltacs ${AMD_LIBS} ${METIS_LIB} ${LAPACK_LIBS}
+TACS_LD_FLAGS = ${EXTRA_LD_FLAGS} ${TACS_LD_CMD} ${AMD_LIBS} ${METIS_LIB} ${LAPACK_LIBS}
 
 # This is the one rule that is used to compile all the
 # source code in TACS
