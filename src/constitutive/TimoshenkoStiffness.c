816dfac8811ac0869f300c881eccc082965d0bda
Komahan Boopathy <komahan.cool@gmail.com>
2017-10-13 16:45:10 -0400

    implemented new constructor for Timoshenko stiffness and computed the stiffness matrix

--- src/constitutive/TimoshenkoStiffness.c
+++ src/constitutive/TimoshenkoStiffness.c
@@ -7,6 +7,75 @@ const char *TimoshenkoStiffness::constitutiveName(){
 }
 
 /*
+  Constructor for Timoshenko beam theory based constitutive object.
+  EA               : axial stiffness
+  EI22, EI33, EI23 : bending stiffness
+  GJ               : torsional stiffness
+  kG22, kG33, kG23 : shearing stiffness
+  m00              : mass per unit span 
+  m11, m22, m33    : moments of inertia such that m11 = m22 + m33
+  xm2, xm3         : cross sectional center of mass location
+  xc2, xc3         : cross sectional centroid
+  xk2, xk3         : cross sectional shear center
+  muS              : viscous damping coefficient
+*/
+TimoshenkoStiffness::TimoshenkoStiffness( const TacsScalar _axis[],
+                                          TacsScalar EA, 
+                                          TacsScalar EI22, TacsScalar EI33, TacsScalar EI23,
+                                          TacsScalar GJ,
+                                          TacsScalar kG22, TacsScalar kG33, TacsScalar kG23,
+                                          TacsScalar m00,
+                                          TacsScalar m11, TacsScalar m22, TacsScalar m33,
+                                          TacsScalar xm2, TacsScalar xm3,
+                                          TacsScalar xc2, TacsScalar xc3,
+                                          TacsScalar xk2, TacsScalar xk3,
+                                          TacsScalar muS ){
+  // Set the reference axis and normalize it
+  axis[0] = _axis[0];
+  axis[1] = _axis[1];
+  axis[2] = _axis[2];
+  TacsScalar tmp = 1.0/sqrt(axis[0]*axis[0] + 
+                            axis[1]*axis[1] + 
+                            axis[2]*axis[2]);
+  axis[0] *= tmp;
+  axis[1] *= tmp;
+  axis[2] *= tmp;
+
+  // Set the entries of the stiffness matrix
+  memset(C, 0, 36*sizeof(TacsScalar));
+
+  // row 1 for axial force
+  C[0] = EA;
+  C[2] = xc3*EA;
+  C[3] = -xc2*EA;
+
+  // row 2 for twisting moment
+  C[7] = GJ + xk2*xk2*kG33 + xk3*xk3*kG22 + 2.0*xk2*xk3*kG23; 
+  C[10] = -xk2*kG23 - xk3*kG22; 
+  C[11] = xk2*kG33 + xk3*kG23; 
+
+  // row 3 for bending moment about axis 2
+  C[12] = C[2];
+  C[14] = EI22 + xc3*xc3*EA; 
+  C[15] = -(EI23 + xc2*xc3*EA);
+  
+  // row 4 for bending moment about axis 3
+  C[18] = C[3];
+  C[20] = C[15];
+  C[21] = EI33 + xc2*xc2*EA;   
+
+  // row 5 for shear 2
+  C[25] = C[10];
+  C[28] = kG22;
+  C[29] = -kG23;
+
+  // row 6 for shear 3
+  C[31] = C[11];
+  C[34] = C[29];
+  C[35] = kG33;
+}
+
+/*
   Set the diagonal components of the stiffness matrix and the mass
   moments of the cross-section.
 */
--- src/constitutive/TimoshenkoStiffness.h
+++ src/constitutive/TimoshenkoStiffness.h
@@ -9,6 +9,17 @@
 
 class TimoshenkoStiffness : public TACSConstitutive {
  public:
+  TimoshenkoStiffness( const TacsScalar _axis[],
+                       TacsScalar EA, 
+                       TacsScalar EI22, TacsScalar EI33, TacsScalar EI23,
+                       TacsScalar GJ,
+                       TacsScalar kG22, TacsScalar kG33, TacsScalar kG23,
+                       TacsScalar m00,
+                       TacsScalar m11, TacsScalar m22, TacsScalar m33,
+                       TacsScalar xm2, TacsScalar xm3,
+                       TacsScalar xc2, TacsScalar xc3,
+                       TacsScalar xk2, TacsScalar xk3,
+                       TacsScalar muS );
   TimoshenkoStiffness( TacsScalar rhoA, TacsScalar rhoIy,
                        TacsScalar rhoIz, TacsScalar rhoIyz,
                        TacsScalar EA, TacsScalar GJ,
