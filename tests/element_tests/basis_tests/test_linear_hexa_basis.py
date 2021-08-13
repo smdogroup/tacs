376b632ae5fb3cac272f1025dedf9e9813a08ef2
trbrooks <tim.r.brooks@nasa.gov>
2021-08-13 09:55:09 -0400

    Added hexa and tetrahedral basis unit tests

---
+++ tests/element_tests/basis_tests/test_linear_hexa_basis.py
@@ -0,0 +1,39 @@
+from tacs import TACS, elements
+import numpy as np
+import unittest
+
+
+class BasisTest(unittest.TestCase):
+    def setUp(self):
+        # fd/cs step size
+        if TACS.dtype is complex:
+            self.dh = 1e-50
+            self.rtol = 1e-11
+        else:
+            self.dh = 1e-6
+            self.rtol = 1e-2
+
+        # Basically, only check relative tolerance
+        self.atol = 1e99
+        self.print_level = 0
+
+        # Set the simulation time
+        self.elem_index = 0
+
+        # Create the basis functions for 3D
+        self.basis = elements.LinearHexaBasis()
+
+    def testElementBasisFunctions(self):
+        fail = elements.TestElementBasisFunctions(self.basis, self.dh, self.print_level,
+                                                  self.atol, self.rtol)
+        self.assertFalse(fail)
+
+    def testElementBasisFaceNormals(self):
+        fail = elements.TestElementBasisFaceNormals(self.basis, self.dh, self.print_level,
+                                                    self.atol, self.rtol)
+        self.assertFalse(fail)
+
+    def testElementBasisJacobianTransform(self):
+        fail = elements.TestElementBasisJacobianTransform(self.basis, self.dh, self.print_level,
+                                                          self.atol, self.rtol)
+        self.assertFalse(fail)
