a67765fd70dfdd761e0a99e098edcbdfd0c67452
trbrooks <tim.r.brooks@nasa.gov>
2021-10-25 12:31:46 -0400

    Adding pytacs examples

---
+++ examples/slot_3d/analysis.py
@@ -0,0 +1,68 @@
+"""
+Structural model of a slot made from 3D tetrahedral elements.
+The slot is constrained at its bolt holes and a set of point
+forces are applied at the top of the slot.
+"""
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+from __future__ import print_function
+import os
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+from pprint import pprint
+from mpi4py import MPI
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+from tacs import functions, pyTACS
+
+comm = MPI.COMM_WORLD
+
+# Instantiate FEASolver
+structOptions = {
+    'varsPerNode':3,
+}
+
+bdfFile = os.path.join(os.path.dirname(__file__), 'slot.bdf')
+# Load BDF file
+FEASolver = pyTACS(bdfFile, options=structOptions)
+# Set up TACS Assembler
+# Don't need a elemCallBack since property info exists in bdf
+FEASolver.createTACSAssembler()
+
+# Add Functions
+FEASolver.addFunction('mass', functions.StructuralMass)
+FEASolver.addFunction('ks_vmfailure', functions.KSFailure, safetyFactor=1.5,
+                      KSWeight=100.0, funcType=1)
+
+# ==============================================================================
+# Setup static problem
+# ==============================================================================
+# Static problem
+evalFuncs = ['mass', 'ks_vmfailure']
+# Read in forces from BDF and create tacs static problems
+SPs = FEASolver.createTACSProbsFromBDF()
+
+# Solve state
+for spID in SPs:
+    FEASolver(SPs[spID])
+
+# Evaluate functions
+funcs = {}
+for spID in SPs:
+    FEASolver.evalFunctions(SPs[spID], funcs, evalFuncs=evalFuncs)
+
+if comm.rank == 0:
+    pprint(funcs)
+
+funcsSens = {}
+for spID in SPs:
+    FEASolver.evalFunctionsSens(SPs[spID], funcsSens, evalFuncs=evalFuncs)
+if comm.rank == 0:
+    pprint(funcsSens)
+
+FEASolver.writeSolution(outputDir=os.path.dirname(__file__))
---
+++ examples/thermal_plate/analysis.py
@@ -0,0 +1,107 @@
+"""
+The nominal case is a heat conduction problem of a
+1m radius plate with a Dirichilet boundary condition applied at the edges,
+such that:
+    T(theta) = T0 + dT * sin(2*theta)
+    T0 = 70 C
+    dT = 30 C
+The problem is then to solve for the temperature within the boundaries of the plate.
+The problem basically boils down to Laplaces problem:
+    grad**2 T = 0
+"""
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+from __future__ import print_function
+import os
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+from pprint import pprint
+
+import numpy as np
+from mpi4py import MPI
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+from tacs import functions, constitutive, elements, TACS, pyTACS, problems
+# Radius of plate
+R = 1.0
+# Area of plate
+area = np.pi * R ** 2
+
+comm = MPI.COMM_WORLD
+
+# Instantiate FEASolver
+structOptions = {
+    'varsPerNode':1,
+    'printtimings':True,
+    # Specify what type of elements we want in the f5
+    'writeSolution':True,
+    'outputElement': TACS.PLANE_STRESS_ELEMENT,
+}
+
+bdfFile = os.path.join(os.path.dirname(__file__), 'circ-plate-dirichlet-bcs.bdf')
+FEASolver = pyTACS(bdfFile, options=structOptions)
+
+def elemCallBack(dvNum, compID, compDescript, elemDescripts, specialDVs, **kwargs):
+    # Material properties
+    rho = 2500.0        # density kg/m^3
+    kappa = 230.0       # Thermal conductivity W/(m⋅K)
+
+    # Plate geometry
+    tplate = 0.005    # 1 mm
+
+    # Setup property and constitutive objects
+    prop = constitutive.MaterialProperties(rho=rho, kappa=kappa)
+    # Set one thickness dv for every component
+    con = constitutive.PlaneStressConstitutive(prop, t=tplate, tNum=dvNum)
+
+    # For each element type in this component,
+    # pass back the appropriate tacs element object
+    elemList = []
+    model = elements.HeatConduction2D(con)
+    for elemDescript in elemDescripts:
+        if elemDescript in ['CQUAD4', 'CQUADR']:
+            basis = elements.LinearQuadBasis()
+        elif elemDescript in ['CTRIA3', 'CTRIAR']:
+            basis = elements.LinearTriangleBasis()
+        else:
+            print("Uh oh, '%s' not recognized" % (elemDescript))
+        elem = elements.Element2D(model, basis)
+        elemList.append(elem)
+
+    # Add scale for thickness dv
+    scale = [100.0]
+    return elemList, scale
+
+# Set up constitutive objects and elements
+FEASolver.createTACSAssembler(elemCallBack)
+
+# Add Functions
+FEASolver.addFunction('mass', functions.StructuralMass)
+FEASolver.addFunction('ks_temp', functions.KSTemperature,
+                      KSWeight=100.0)
+FEASolver.addFunction('avg_temp', functions.AverageTemperature, volume=area)
+
+# Structural problem
+evalFuncs = ['mass', 'ks_temp', 'avg_temp']
+sp = problems.StaticProblem(name='plate', evalFuncs=evalFuncs)
+
+# Solve state
+FEASolver(sp)
+
+# Evaluate functions
+funcs = {}
+FEASolver.evalFunctions(sp, funcs)
+if comm.rank == 0:
+    pprint(funcs)
+
+funcsSens = {}
+FEASolver.evalFunctionsSens(sp, funcsSens)
+if comm.rank == 0:
+    pprint(funcsSens)
+
+FEASolver.writeSolution(outputDir=os.path.dirname(__file__))
