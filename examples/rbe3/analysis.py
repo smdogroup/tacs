e277faa7f730e28ad8488176a57f0130af0238a8
trbrooks <tim.r.brooks@nasa.gov>
2021-11-19 15:35:23 -0500

    Updating rbe example with something more illustrative

--- examples/rbe3/analysis.py
+++ examples/rbe3/analysis.py
@@ -1,7 +1,6 @@
 """
-RBE3 example where an RBE3 is used to connect two separate cantilevered plate sections.
-A load is applied at the centroid of the RBE3. NOTE: This is the same case as the RBE2
-example, with the RBE2 replaced with an RBE3.
+Cylindrical beam constructed from shell elements. The beam is cantilevered at
+one end and loaded at the other using an RBE3.
 """
 # ==============================================================================
 # Standard Python modules
@@ -10,36 +9,23 @@ from __future__ import print_function
 import os
 
 # ==============================================================================
-# External Python modules
-# ==============================================================================
-from pprint import pprint
-from mpi4py import MPI
-import numpy
-
-# ==============================================================================
 # Extension modules
 # ==============================================================================
 from tacs import pyTACS
 
-comm = MPI.COMM_WORLD
-
-# Instantiate FEAAssembler
-structOptions = {
-    'printtiming':True,
-}
+structOptions = {'writeSolution': True, }
 
 bdfFile = os.path.join(os.path.dirname(__file__), 'rbe3.bdf')
-FEAAssembler = pyTACS(bdfFile, comm, options=structOptions)
+# Load BDF file
+FEAAssembler = pyTACS(bdfFile, options=structOptions)
 # Set up TACS Assembler
 # Don't need a elemCallBack since property info exists in bdf
 FEAAssembler.initialize()
 
-# Read in forces from BDF and create tacs static problems
+# Read in forces from BDF and create tacs struct problems
 SPs = FEAAssembler.createTACSProbsFromBDF()
 
-# Solve problems
+# Solve each structural problem and write solutions
 for caseID in SPs:
     SPs[caseID].solve()
     SPs[caseID].writeSolution(outputDir=os.path.dirname(__file__))
-
-
