cf655424d6d8eb99bab17f68e74b18cb205ec750
trbrooks <tim.r.brooks@nasa.gov>
2022-04-15 16:40:03 -0400

    Adding complex conda build

--- conda/build.sh
+++ conda/build.sh
@@ -1,5 +1,5 @@
 export TACS_DIR=${SRC_DIR}
-#export CONDA_BUILD_SYSROOT=${HOME}/opt/MacOSX10.10.sdk
+
 if [[ $(uname) == Darwin ]]; then
   export SO_EXT="dylib"
   export SO_LINK_FLAGS="-fPIC -dynamiclib"
@@ -14,14 +14,23 @@ elif [[ "$target_platform" == linux-* ]]; then
   export LAPACK_LIBS="-L${PREFIX}/lib/ -llapack -lpthread -lblas"
 fi
 
+if [[ $scalar == "complex" ]]; then
+  alias MAKE="make complex"
+  alias PIP="CFLAGS=-DTACS_USE_COMPLEX ${PYTHON} -m pip"
+elif [[ $mpi == "mpich" ]]; then
+  alias MAKE="make default"
+  alias PIP=" ${PYTHON} -m pip"
+fi
+
 cp Makefile.in.info Makefile.in;
-make default TACS_DIR=${TACS_DIR} \
-             LAPACK_LIBS="${LAPACK_LIBS}" \
-             METIS_INCLUDE=-I${PREFIX}/include/ METIS_LIB="-L${PREFIX}/lib/ -lmetis" \
-             SO_LINK_FLAGS="${LIB_SLF}" SO_EXT=${SO_EXT};
+MAKE TACS_DIR=${TACS_DIR} \
+     LAPACK_LIBS="${LAPACK_LIBS}" \
+     METIS_INCLUDE=-I${PREFIX}/include/ METIS_LIB="-L${PREFIX}/lib/ -lmetis" \
+     SO_LINK_FLAGS="${LIB_SLF}" SO_EXT=${SO_EXT};
 mv ${TACS_DIR}/lib/libtacs.${SO_EXT} ${PREFIX}/lib;
-${PYTHON} -m pip install --no-deps . -vv;
+PIP install --no-deps --prefix=${PREFIX} . -vv;
 cd ${TACS_DIR}/extern/f5tovtk;
+# purposely lower-case make, only default supported below
 make default TACS_DIR=${TACS_DIR} \
              LAPACK_LIBS="${LAPACK_LIBS}" \
              METIS_INCLUDE=-I${PREFIX}/include/ METIS_LIB="-L${PREFIX}/lib/ -lmetis" \
