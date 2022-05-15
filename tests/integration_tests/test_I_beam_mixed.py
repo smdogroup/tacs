4bbafc2d394b0269bebe89c5338b37f6ca06984c
trbrooks <tim.r.brooks@nasa.gov>
2022-05-15 18:25:44 -0400

    Adding I-beam integration test

---
+++ tests/integration_tests/test_I_beam_mixed.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os
+from tacs import pytacs, elements, constitutive, functions
+from pytacs_analysis_base_test import PyTACSTestCase
+
+'''
+This model tests an I-beam made up of two different element types:
+    - Quad shell elements are used to model the web
+    - Axial rod elements are used to model the caps
+The properties of the cross-section are given below:
+    Acaps = 0.05
+    tweb = 0.1
+We apply apply various tip loads test KSDisplacement, KSFailure, 
+StructuralMass, and Compliance functions and sensitivities.
+'''
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+bdf_file = os.path.join(base_dir, "./input_files/I_beam.bdf")
+
+FUNC_REFS = {'load_set_000_compliance': 100430.19372545928,
+             'load_set_000_ks_vmfailure': 0.4486594909084844,
+             'load_set_000_mass': 5400.0,
+             'load_set_000_x_disp': 0.3558707399683029,
+             'load_set_000_y_disp': 1.2614949438778236,
+             'load_set_000_z_disp': 0.34011973816621555}
+
+ksweight = 10.0
+
+class ProblemTest(PyTACSTestCase.PyTACSTest):
+    N_PROCS = 2  # this is how many MPI processes to use for this TestCase.
+
+    def setup_pytacs(self, comm, dtype):
+        """
+        Setup mesh and pytacs object for problem we will be testing.
+        """
+
+        # Overwrite default check values
+        if dtype == complex:
+            self.rtol = 1e-6
+            self.atol = 1e-6
+            self.dh = 1e-50
+        else:
+            self.rtol = 2e-1
+            self.atol = 1e-3
+            self.dh = 1e-6
+
+        # Instantiate FEA Assembler
+        struct_options = {}
+
+        fea_assembler = pytacs.pyTACS(bdf_file, comm, options=struct_options)
+
+        # Set up constitutive objects and elements
+        fea_assembler.initialize()
+
+        return fea_assembler
+
+    def setup_tacs_vecs(self, fea_assembler, dv_pert_vec, xpts_pert_vec):
+        """
+        Setup user-defined vectors for analysis and fd/cs sensitivity verification
+        """
+        # Create temporary dv vec for doing fd/cs
+        dv_pert_vec[:] = 1.0
+
+        # Define perturbation array that moves all nodes on shell
+        xpts = fea_assembler.getOrigNodes()
+        xpts_pert_vec[:] = xpts
+
+        return
+
+    def setup_funcs(self, fea_assembler, problems):
+        """
+        Create a list of functions to be tested and their reference values for the problem
+        """
+        # Add Functions
+        for problem in problems:
+            problem.addFunction('mass', functions.StructuralMass)
+            problem.addFunction('compliance', functions.Compliance)
+            problem.addFunction('ks_vmfailure', functions.KSFailure,
+                                ksWeight=ksweight)
+            problem.addFunction('x_disp', functions.KSDisplacement,
+                                ksWeight=ksweight, direction=[10.0, 0.0, 0.0])
+            problem.addFunction('y_disp', functions.KSDisplacement,
+                                ksWeight=ksweight, direction=[0.0, 10.0, 0.0])
+            problem.addFunction('z_disp', functions.KSDisplacement,
+                                ksWeight=ksweight, direction=[0.0, 0.0, 10.0])
+        func_list = ['mass', 'compliance', 'x_disp', 'y_disp', 'z_disp']
+        return func_list, FUNC_REFS
+
+    def setup_tacs_problems(self, fea_assembler):
+        """
+        Setup pytacs object for problems we will be testing.
+        """
+        # Read in forces from BDF and create tacs struct problems
+        tacs_probs = fea_assembler.createTACSProbsFromBDF()
+        # Convert from dict to list
+        tacs_probs = tacs_probs.values()
+        # Set convergence to be tight for test
+        for problem in tacs_probs:
+            problem.setOption('L2Convergence', 1e-20)
+            problem.setOption('L2ConvergenceRel', 1e-20)
+
+        return tacs_probs
