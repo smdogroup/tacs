1c2ca185c02b4e67ce8fe357131c1e50b97ff12e
trbrooks <tim.r.brooks@nasa.gov>
2022-05-17 05:29:39 -0400

    Adding more comments to beam_opt

--- examples/beam/beam_opt.py
+++ examples/beam/beam_opt.py
@@ -1,7 +1,22 @@
 """
-Mass minimization of uCRM wingbox subject to a constant vertical force
+This example demonstrates TACS structural optimization capabilities.
+The beam model that we will be using for this problem is a rectangular beam,
+cantilevered, with a shear load applied at the tip. The beam is discretized using
+100 beam elements along it's span.
+
+The optimization problem is as follows:
+Minimize the mass of the beam with respect to the thickness of each element,
+subject to a max stress constraint dictated by the materials yield stress.
+
+By realizing that the stress at any spanwise cross-section in the beam
+can be found independently using:
+    sigma(x,y) = y*M(x)/I
+An analytical solution for this problem can be shown to be:
+    t(x) = sqrt(6*V*(L-x)/(w*sigma_y))
+
+The optimization is setup using TACS' MPHYS module, which acts as a wrapper
+for OpenMDAO.
 """
-from __future__ import division, print_function
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +32,7 @@ from tacs import elements, constitutive, functions
 bdf_file = os.path.join(os.path.dirname(__file__), 'beam_opt.bdf')
 
 # Beam thickness (initial)
-t = 0.01            # m
+t = 0.05            # m
 # Beam width
 w = 0.05        # m
 # Length of beam
@@ -67,42 +82,53 @@ class Top(Multipoint):
                         'problem_setup': problem_setup,
                         'mesh_file': bdf_file}
 
+        # Initialize MPHYS builder for TACS
         struct_builder = TacsBuilder(tacs_options, coupled=False, write_solution=False)
         struct_builder.initialize(self.comm)
         dv_array = struct_builder.get_initial_dvs()
 
+        # Create independent variable component to control design variables for problem
         dvs = self.add_subsystem('dvs', om.IndepVarComp(), promotes=['*'])
         dvs.add_output('dv_struct', dv_array)
 
+        # Add mesh component
         self.add_subsystem('mesh', struct_builder.get_mesh_coordinate_subsystem())
         self.mphys_add_scenario('tip_shear', ScenarioStructural(struct_builder=struct_builder))
         self.mphys_connect_scenario_coordinate_source('mesh', 'tip_shear', 'struct')
 
+        # Connect dv component to input of structural scenario
         self.connect('dv_struct', 'tip_shear.dv_struct')
 
 
 ################################################################################
 # OpenMDAO setup
 ################################################################################
-
+# Instantiate OpenMDAO problem
 prob = om.Problem()
 prob.model = Top()
 model = prob.model
 
+# Declare design variables, objective, and constraint
 model.add_design_var('dv_struct', lower=0.001, upper=0.1, scaler=100.0)
 model.add_objective('tip_shear.mass', scaler=1.0)
 model.add_constraint('tip_shear.ks_vmfailure', lower=0.0, upper=1.0, scaler=1.0)
 
+# Configure optimizer
 prob.driver = om.ScipyOptimizeDriver(debug_print=['objs', 'nl_cons'], maxiter=1000)
 prob.driver.options['optimizer'] = 'SLSQP'
 
+# Setup OpenMDAO problem
 prob.setup()
 
+# Run optimization
 prob.run_driver()
 
+# Get optimized solution variables
 x = prob.get_val('mesh.x_struct0', get_remote=True)[:-3:3]
 t_opt = prob['dv_struct']
 m_opt = prob['tip_shear.mass']
+
+# Get analytical solution
 t_exact = np.sqrt(6*(L-x)*V/w/ys)
 
 if __name__ == "__main__" and prob.comm.size == 1:
@@ -111,7 +137,7 @@ if __name__ == "__main__" and prob.comm.size == 1:
 
     # Plot results for solution
     plt.plot(x, t_opt, 'o', x, t_exact)
-    plt.legend(['opt', 'exact'])
+    plt.legend(['optimized', 'analytical'])
     plt.ylabel('t(x)')
     plt.xlabel('x')
     plt.title('Optimal beam thickness profile')
