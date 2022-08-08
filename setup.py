24d24269af071b5427cae6499ee3a1e0496c7189
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2022-08-08 08:55:04 -0400

    Making minimum required mpi4py 3.1.0 (#122)
    * Making minimum required mpi4py 3.1.0
    
    * Adding minimum version for testflo

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ with open(os.path.join(tacs_root, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 optional_dependencies = {
-    'testing': ['testflo'],
+    'testing': ['testflo>=1.4.7'],
     'docs': ['sphinx', 'breathe', 'sphinxcontrib-programoutput'],
     'mphys': ['mphys>=0.4.0', 'openmdao'],
 }
@@ -97,9 +97,8 @@ setup(name='tacs',
       author='Graeme J. Kennedy',
       author_email='graeme.kennedy@ae.gatech.edu',
       install_requires=[
-          # Make sure the user's numpy version are at least more current than the build versions
-          f'numpy>={numpy.__version__}',
-          'mpi4py>=3.0.2',
+          'numpy',
+          'mpi4py>=3.1.0',
           'scipy>=1.2.1',
           'pynastran>=1.3.3'
       ],
