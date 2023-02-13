fixed sens file printouts and caps shape derivative tests

tests/integration_tests/test_caps_shape_derivatives.py
@@ -1,119 +1,208 @@
 """
-Sean Engelstad, October 2022
+Sean Engelstad, Febuary 2022
 GT SMDO Lab, Dr. Graeme Kennedy
 Caps to TACS example
 """
 
 import unittest, os, numpy as np, importlib
-from tacs import functions, caps2tacs, TACS
+from tacs import caps2tacs, TACS
 from mpi4py import MPI
 
 caps_loader = importlib.util.find_spec("pyCAPS")
 complex_mode = TACS.dtype == complex
+complex_mode = False
+
 
 # only run the test if pyCAPS can be imported
-@unittest.skipIf(caps_loader is None or complex_mode, "skipping ESP/CAPS test without pyCAPS module or in real mode")
-class TestCaps2Tacs(unittest.TestCase):
-    def _build_tacs_aim(self):
+@unittest.skipIf(
+    caps_loader is None or complex_mode,
+    "skipping ESP/CAPS test without pyCAPS module or in real mode",
+)
+class TestCaps2TacsShape(unittest.TestCase):
+    def test_wing_shape_derivatives(self):
+        """
+        test the shape derivatives from ESP/CAPS into TACS forward & adjoint analysis
+        """
+
+        # build the tacs model with constraints, loads, properties, analysis functions, mesh, etc.
         comm = MPI.COMM_WORLD
         csm_path = os.path.join("input_files", "simple_naca_wing.csm")
-        caps_struct = caps2tacs.CapsStruct.build(csm_file=csm_path, comm=comm)
-        tacs_aim = caps_struct.tacs_aim
-        caps_struct.egads_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
+        tacs_model = caps2tacs.TacsModel.build(csm_file=csm_path, comm=comm)
+        tacs_model.egads_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
             edge_pt_min=15,
             edge_pt_max=20,
             global_mesh_size=0.1,
             max_surf_offset=0.01,
             max_dihedral_angle=5,
         ).register_to(
-            tacs_aim
+            tacs_model
         )
-
-        aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_aim)
+        aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_model)
 
         # setup the thickness design variables + automatic shell properties
-        nribs = int(tacs_aim.get_config_parameter("nribs"))
-        nspars = int(tacs_aim.get_config_parameter("nspars"))
+        nribs = int(tacs_model.get_config_parameter("nribs"))
+        nspars = int(tacs_model.get_config_parameter("nspars"))
         for irib in range(1, nribs + 1):
             caps2tacs.ShellProperty(
                 caps_group=f"rib{irib}", material=aluminum, membrane_thickness=0.05
-            ).register_to(tacs_aim)
+            ).register_to(tacs_model)
         for ispar in range(1, nspars + 1):
             caps2tacs.ShellProperty(
                 caps_group=f"spar{ispar}", material=aluminum, membrane_thickness=0.05
-            ).register_to(tacs_aim)
+            ).register_to(tacs_model)
         caps2tacs.ShellProperty(
             caps_group="OML", material=aluminum, membrane_thickness=0.03
-        ).register_to(tacs_aim)
+        ).register_to(tacs_model)
 
-        # register one shape variable rib_a1
-        caps2tacs.ShapeVariable("rib_a1").register_to(tacs_aim)
+        # include shape variables
+        caps2tacs.ShapeVariable("rib_a1").register_to(tacs_model)
+        # caps2tacs.ShapeVariable("rib_a2").register_to(tacs_model)
 
         # add constraints and loads
-        caps2tacs.PinConstraint("root").register_to(tacs_aim)
+        caps2tacs.PinConstraint("root").register_to(tacs_model)
         caps2tacs.GridForce("OML", direction=[0, 0, 1.0], magnitude=100).register_to(
-            tacs_aim
+            tacs_model
         )
 
-        # run the pre analysis to build tacs input files
-        self.tacs_aim_wrapper = tacs_aim.setup_aim()
-        self.tacs_aim = tacs_aim.aim  # get actual aim from underneath the wrapper
+        # add analysis functions
+        caps2tacs.AnalysisFunction.mass().register_to(tacs_model)
+        # caps2tacs.AnalysisFunction.ksfailure().register_to(tacs_model)
 
-    def _run_analysis(self):
-        """
-        run a complete forward and adjoint analysis
-        """
-        self.tacs_aim.preAnalysis()
-        SPs = self.tacs_aim_wrapper.createTACSProbs()
-        for caseID in SPs:
-            SPs[caseID].addFunction("mass", functions.StructuralMass)
-            SPs[caseID].addFunction(
-                "ks_vmfailure", functions.KSFailure, safetyFactor=1.5, ksWeight=50.0
-            )
+        # setup the tacs model
+        tacs_model.setup()
 
-        # solve the forward and adjoint analysis for each struct problem
-        self._func_names = ["mass", "ks_vmfailure"]
-        for caseID in SPs:
-            SPs[caseID].solve()
-            SPs[caseID].writeSensFile(
-                evalFuncs=self._func_names,
-                tacsAim=self.tacs_aim_wrapper,
-            )
+        # perform the derivative test with these random covariant and contravariant tensors
+        dLdf = {func_key: np.random.rand() for func_key in tacs_model.function_names}
+        dxds = {var.name: np.random.rand() for var in tacs_model.variables}
 
-        # compute the shape derivatives in ESP/CAPS which reads the sens file
-        self.tacs_aim.postAnalysis()
+        # total derivative using adjoint & coordinate derivatives
+        tacs_model.update_design()  # update design with nothing to write in shape vars for next design
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
+        adjoint_TD = 0.0
+        initial = {}
+        for func in tacs_model.analysis_functions:
+            initial[func.name] = func.value
+            for var in tacs_model.variables:
+                derivative = func.get_derivative(var)
+                adjoint_TD += dLdf[func.name] * derivative * dxds[var.name]
 
-        # functions and gradients are stored in the tacs AIM dynout method
+        # total derivative with finite difference
+        h = 1.0e-5
+        for (
+            shape_var
+        ) in (
+            tacs_model.shape_variables
+        ):  # perturb the variables to affect update design
+            shape_var.value += dxds[var.name] * h
+        tacs_model.update_design()
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
 
-    def test_mass_shape_derivatives(self):
+        finite_diff_TD = 0.0
+        for func in tacs_model.analysis_functions:
+            deriv = (func.value - initial[func.name]) / h
+            finite_diff_TD += dLdf[func.name] * deriv
+
+        # relative error of total derivatives
+        rel_error = (adjoint_TD - finite_diff_TD) / finite_diff_TD
+        print("\nFD shape derivative test with d(mass)/d(rib_a1,rib_a2)...")
+        print(f"\tAdjoint TD = {adjoint_TD}")
+        print(f"\tFinite Diff TD = {finite_diff_TD}")
+        print(f"\trelative error = {rel_error}")
+
+        self.assertTrue(abs(rel_error) < 1.0e-4)
+
+    def test_wing_shape_and_thick_derivatives(self):
         """
         test the shape derivatives from ESP/CAPS into TACS forward & adjoint analysis
         """
 
-        test_functions = ["mass", "ks_vmfailure"]
-        dLdf = {func_key: np.random.rand() for func_key in test_functions}
+        # build the tacs model with constraints, loads, properties, analysis functions, mesh, etc.
+        comm = MPI.COMM_WORLD
+        csm_path = os.path.join("input_files", "simple_naca_wing.csm")
+        tacs_model = caps2tacs.TacsModel.build(csm_file=csm_path, comm=comm)
+        tacs_model.egads_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
+            edge_pt_min=15,
+            edge_pt_max=20,
+            global_mesh_size=0.1,
+            max_surf_offset=0.01,
+            max_dihedral_angle=5,
+        ).register_to(
+            tacs_model
+        )
+        aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_model)
+
+        # setup the thickness design variables + automatic shell properties
+        nribs = int(tacs_model.get_config_parameter("nribs"))
+        nspars = int(tacs_model.get_config_parameter("nspars"))
+        for irib in range(1, nribs + 1):
+            caps2tacs.ThicknessVariable(
+                caps_group=f"rib{irib}", value=0.05, material=aluminum
+            ).register_to(tacs_model)
+        for ispar in range(1, nspars + 1):
+            caps2tacs.ThicknessVariable(
+                caps_group=f"spar{ispar}", value=0.05, material=aluminum
+            ).register_to(tacs_model)
+        caps2tacs.ThicknessVariable(
+            caps_group="OML", value=0.03, material=aluminum
+        ).register_to(tacs_model)
+
+        # include shape variables
+        caps2tacs.ShapeVariable("rib_a1").register_to(tacs_model)
+        caps2tacs.ShapeVariable("rib_a2").register_to(tacs_model)
+
+        # add constraints and loads
+        caps2tacs.PinConstraint("root").register_to(tacs_model)
+        caps2tacs.GridForce("OML", direction=[0, 0, 1.0], magnitude=100).register_to(
+            tacs_model
+        )
+
+        # add analysis functions
+        caps2tacs.AnalysisFunction.mass().register_to(tacs_model)
+        # caps2tacs.AnalysisFunction.ksfailure().register_to(tacs_model)
+
+        # setup the tacs model
+        tacs_model.setup()
+
+        # perform the derivative test with these random covariant and contravariant tensors
+        dLdf = {func_key: np.random.rand() for func_key in tacs_model.function_names}
+        dxds = {var.name: np.random.rand() for var in tacs_model.variables}
 
         # total derivative using adjoint & coordinate derivatives
-        self._build_tacs_aim()
-        self._run_analysis()
+        tacs_model.update_design()  # update design with nothing to write in shape vars for next design
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
         adjoint_TD = 0.0
         initial = {}
-        for func in test_functions:
-            adjoint_TD += dLdf[func] * self.tacs_aim.dynout[func].deriv("rib_a1")
-            initial[func] = self.tacs_aim.dynout[func].value
+        for func in tacs_model.analysis_functions:
+            initial[func.name] = func.value
+            for var in tacs_model.variables:
+                derivative = func.get_derivative(var)
+                adjoint_TD += dLdf[func.name] * derivative * dxds[var.name]
 
         # total derivative with finite difference
         h = 1.0e-5
-        self.tacs_aim.geometry.despmtr["rib_a1"].value += h
-        self._run_analysis()
+        for (
+            var
+        ) in tacs_model.variables:  # perturb the variables to affect update design
+            var.value += dxds[var.name] * h
+        tacs_model.update_design()
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
+
         finite_diff_TD = 0.0
-        for func in test_functions:
-            deriv = (self.tacs_aim.dynout[func].value - initial[func]) / h
-            finite_diff_TD += dLdf[func] * deriv
+        for func in tacs_model.analysis_functions:
+            deriv = (func.value - initial[func.name]) / h
+            finite_diff_TD += dLdf[func.name] * deriv
 
         # relative error of total derivatives
         rel_error = (adjoint_TD - finite_diff_TD) / finite_diff_TD
-        print("\nFD shape derivative test with d(mass,ksfailure)/drib_a1...")
+        print("\nFD shape+thick derivative test with d(mass)/d(rib_a1,rib_a2)...")
         print(f"\tAdjoint TD = {adjoint_TD}")
         print(f"\tFinite Diff TD = {finite_diff_TD}")
         print(f"\trelative error = {rel_error}")

tests/integration_tests/test_caps_thick_derivatives.py
@@ -1,5 +1,5 @@
 """
-Sean Engelstad, October 2022
+Sean Engelstad, Febuary 2022
 GT SMDO Lab, Dr. Graeme Kennedy
 Caps to TACS example
 """
@@ -11,123 +11,101 @@ from mpi4py import MPI
 caps_loader = importlib.util.find_spec("pyCAPS")
 complex_mode = TACS.dtype == complex
 
+
 # only run the test if pyCAPS can be imported
 @unittest.skipIf(caps_loader is None, "skipping ESP/CAPS test without pyCAPS module")
 class TestCaps2Tacs(unittest.TestCase):
-    def _build_tacs_aim(self):
+    def test_thickness_derivatives(self):
+        """
+        test the thickness derivatives in TACS setup from ESP/CAPS
+        """
+
+        # build the tacs model with constraints, loads, properties, analysis functions, mesh, etc.
         comm = MPI.COMM_WORLD
         csm_path = os.path.join("input_files", "simple_naca_wing.csm")
-        caps_struct = caps2tacs.CapsStruct.build(csm_file=csm_path, comm=comm)
-        tacs_aim = caps_struct.tacs_aim
-        caps_struct.egads_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
+        tacs_model = caps2tacs.TacsModel.build(csm_file=csm_path, comm=comm)
+        tacs_model.egads_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
             edge_pt_min=15,
             edge_pt_max=20,
             global_mesh_size=0.1,
             max_surf_offset=0.01,
             max_dihedral_angle=5,
         ).register_to(
-            tacs_aim
+            tacs_model
         )
-
-        aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_aim)
+        aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_model)
 
         # setup the thickness design variables + automatic shell properties
-        nribs = int(tacs_aim.get_config_parameter("nribs"))
-        nspars = int(tacs_aim.get_config_parameter("nspars"))
+        nribs = int(tacs_model.get_config_parameter("nribs"))
+        nspars = int(tacs_model.get_config_parameter("nspars"))
         for irib in range(1, nribs + 1):
             caps2tacs.ThicknessVariable(
                 caps_group=f"rib{irib}", value=0.05, material=aluminum
-            ).register_to(tacs_aim)
+            ).register_to(tacs_model)
         for ispar in range(1, nspars + 1):
             caps2tacs.ThicknessVariable(
                 caps_group=f"spar{ispar}", value=0.05, material=aluminum
-            ).register_to(tacs_aim)
+            ).register_to(tacs_model)
         caps2tacs.ThicknessVariable(
             caps_group="OML", value=0.03, material=aluminum
-        ).register_to(tacs_aim)
+        ).register_to(tacs_model)
 
         # add constraints and loads
-        caps2tacs.PinConstraint("root").register_to(tacs_aim)
+        caps2tacs.PinConstraint("root").register_to(tacs_model)
         caps2tacs.GridForce("OML", direction=[0, 0, 1.0], magnitude=100).register_to(
-            tacs_aim
+            tacs_model
         )
-        # run the pre analysis to build tacs input files
-        self.tacs_aim = tacs_aim.setup_aim().pre_analysis()
-        self.SPs = self.tacs_aim.createTACSProbs()
-        for caseID in self.SPs:
-            self.SPs[caseID].addFunction("mass", functions.StructuralMass)
 
-    def _run_analysis(self):
-        """
-        run a complete forward and adjoint analysis
-        """
+        # add analysis functions
+        caps2tacs.AnalysisFunction.mass().register_to(tacs_model)
+        # caps2tacs.AnalysisFunction.ksfailure().register_to(tacs_model)
 
-        # solve the forward and adjoint analysis for each struct problem
-        self._func_names = ["mass"]
-        tacs_funcs = {}
-        tacs_sens = {}
-        for caseID in self.SPs:
-            self.SPs[caseID].solve()
-            self.SPs[caseID].evalFunctions(tacs_funcs, evalFuncs=self._func_names)
-            self.SPs[caseID].evalFunctionsSens(tacs_sens, evalFuncs=self._func_names)
-            self.SPs[caseID].writeSolution(
-                baseName="tacs_output", outputDir=self.tacs_aim.analysis_dir
-            )
+        # setup the tacs model
+        tacs_model.setup()
 
-        # functions and gradients are stored in the tacs AIM dynout method
-        self._functions = {}
-        self._gradients = {}
-        tacs_key = list(tacs_funcs.keys())[0]
-        self._functions["mass"] = tacs_funcs[tacs_key]
-        self._gradients["mass"] = {}
-        struct_sens = tacs_sens[tacs_key]["struct"]
-        for ithick, thick_var in enumerate(self.tacs_aim.thickness_variables):
-            self._gradients["mass"][thick_var.name] = struct_sens[ithick]
-
-    def test_mass_thickness_derivatives(self):
-        """
-        test the shape derivatives from ESP/CAPS into TACS forward & adjoint analysis
-        """
+        # perform the derivative test with these random covariant and contravariant tensors
+        dLdf = {func_key: np.random.rand() for func_key in tacs_model.function_names}
+        dxds = {var.name: np.random.rand() for var in tacs_model.variables}
 
         # total derivative using adjoint & coordinate derivatives
-        self._build_tacs_aim()
-        self._run_analysis()
-
-        # random dvar/ds contravariant tensor for the complex step perturbation
-        dvar_ds = {
-            thick_var.name: np.random.rand()
-            for thick_var in self.tacs_aim.thickness_variables
-        }
+        tacs_model.update_design()  # update design with nothing to write in shape vars for next design
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
         adjoint_TD = 0.0
-        for thick_var in self.tacs_aim.thickness_variables:
-            adjoint_TD += (
-                self._gradients["mass"][thick_var.name] * dvar_ds[thick_var.name]
-            )
-        adjoint_TD = adjoint_TD.real
+        initial = {}
+        for func in tacs_model.analysis_functions:
+            initial[func.name] = func.value
+            for var in tacs_model.variables:
+                derivative = func.get_derivative(var)
+                adjoint_TD += dLdf[func.name] * derivative * dxds[var.name]
 
         # total derivative with finite difference
         h = 1.0e-5
-        for caseID in self.SPs:
-            xarray = self.SPs[
-                caseID
-            ].x.getArray()  # gets the xarray of last Struct Problem
-            for ithick, thick_var in enumerate(self.tacs_aim.thickness_variables):
-                xarray[ithick] += 1j * dvar_ds[thick_var.name] * h
+        for (
+            var
+        ) in tacs_model.variables:  # perturb the variables to affect update design
+            var.value += dxds[var.name] * h
+        tacs_model.update_design()
+        tacs_model.pre_analysis()
+        tacs_model.run_analysis()
+        tacs_model.post_analysis()  # calls tacsAim.postAnalysis and reads tacsAim.dynout under the hood
 
-        self._run_analysis()
-        complex_step_TD = self._functions["mass"].imag / h
+        finite_diff_TD = 0.0
+        for func in tacs_model.analysis_functions:
+            deriv = (func.value - initial[func.name]) / h
+            finite_diff_TD += dLdf[func.name] * deriv
 
         # relative error of total derivatives
-        rel_error = (adjoint_TD - complex_step_TD) / complex_step_TD
+        rel_error = (adjoint_TD - finite_diff_TD) / finite_diff_TD
         print(
-            "Complex Step thickness derivative test with d(mass,ksfailure)/drib_a1..."
+            "\nFD shape derivative test with d(mass,ksfailure)/d(length,width,stiffHeight)..."
         )
         print(f"\tAdjoint TD = {adjoint_TD}")
-        print(f"\tComplex Step TD = {complex_step_TD}")
+        print(f"\tFinite Diff TD = {finite_diff_TD}")
         print(f"\trelative error = {rel_error}")
 
-        rtol = 1.0e-7 if complex_mode else 1e-4
-        self.assertTrue(abs(rel_error) < rtol)
+        self.assertTrue(abs(rel_error) < 1.0e-4)
 
 
 if __name__ == "__main__":