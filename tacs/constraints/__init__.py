b5a0ad860f5dfdc24b0b9142bf51151b4677dc3d
A-Gray-94 <alachris@umich.edu>
2023-05-19 14:22:23 -0400

    Minor fix

--- tacs/constraints/__init__.py
+++ tacs/constraints/__init__.py
@@ -1,5 +1,6 @@
 from .adjacency import AdjacencyConstraint
 from .dv import DVConstraint
+from .panel_length import PanelLengthConstraint
 from .volume import VolumeConstraint
 from .base import TACSConstraint
 
