3aea51c398e878e51b40e5072c2466ba9ceecd6d
Alasdair Gray <alachris@umich.edu>
2023-06-02 09:10:03 -0400

    Add methods to mphys interface for getting DV bounds and scaling factors (#218)
    * Add methods to mphys interface for getting DV bounds and scaling factors
    
    * Use new methods in MPhys integration test

--- tacs/mphys/mphys_tacs.py
+++ tacs/mphys/mphys_tacs.py
@@ -1125,6 +1125,57 @@ class TacsBuilder(Builder):
         else:
             return self.fea_assembler.getOrigDesignVars()
 
+    def get_dv_bounds(self):
+        """Get arrays containing the lower and upper bounds for the design variables,
+        in the form needed by OpenMDAO's `add_design_variable` method.
+
+        Returns
+        -------
+        list of ndarray
+            lower and upper bounds for the design variables
+        """
+        if MPI is not None and self.comm.size > 1:
+            # Get bounds owned by this processor
+            local_dv_bounds = self.fea_assembler.getDesignVarRange()
+            local_dv_bounds = list(local_dv_bounds)
+            local_dv_bounds[0] = local_dv_bounds[0].astype(float)
+            local_dv_bounds[1] = local_dv_bounds[1].astype(float)
+
+            # Size of design variable on this processor
+            local_ndvs = self.fea_assembler.getNumDesignVars()
+            # Size of design variable vector on each processor
+            dv_sizes = self.comm.allgather(local_ndvs)
+            # Offsets for global design variable vector
+            offsets = np.zeros(self.comm.size, dtype=int)
+            offsets[1:] = np.cumsum(dv_sizes)[:-1]
+            # Gather the portions of the design variable array distributed across each processor
+            tot_ndvs = sum(dv_sizes)
+            global_dv_bounds = []
+            for ii in [0, 1]:
+                global_dv_bounds.append(
+                    np.zeros(tot_ndvs, dtype=local_dv_bounds[ii].dtype)
+                )
+                self.comm.Allgatherv(
+                    local_dv_bounds[ii],
+                    [global_dv_bounds[ii], dv_sizes, offsets, MPI.DOUBLE],
+                )
+            # return the global dv array
+            return global_dv_bounds
+        else:
+            return self.fea_assembler.getDesignVarRange()
+
+    def get_dv_scalers(self):
+        """Get an array containing the scaling factors for the design
+        variables, in the form needed by OpenMDAO's `add_design_variable`
+        method.
+
+        Returns
+        -------
+        array
+            Scaling values
+        """
+        return np.array(self.fea_assembler.scaleList)
+
     def get_ndv(self):
         """
         Get total number of structural design variables across all procs
