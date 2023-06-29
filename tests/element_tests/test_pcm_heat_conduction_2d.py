ba7c33156b5dbd39b5c3f20f005031785192b92f
Alasdair Gray <alachris@umich.edu>
2023-06-29 15:13:08 -0400

    Turn off printing in pcm constitutive test

--- tests/element_tests/test_pcm_heat_conduction_2d.py
+++ tests/element_tests/test_pcm_heat_conduction_2d.py
@@ -16,7 +16,7 @@ class ModelTest(unittest.TestCase):
             self.rtol = 1e-2
 
         self.atol = np.clip(1e-5 * self.rtol, 1e-8, 1e-14)
-        self.print_level = 2
+        self.print_level = 0
 
         # Set element index
         self.elem_index = 0
