749a2c4fa6f7de6f6a1499a28672a2a0adbd5f77
seanfireball1 <sean.engelstad1@gmail.com>
2024-03-10 19:11:58 -0400

    update train models sh

--- examples/buckling_plate/train_models.sh
+++ examples/buckling_plate/train_models.sh
@@ -1,4 +1,4 @@
-#python 4_train_GP_model.py --load Nx --BC SS
+python 4_train_GP_model.py --load Nx --BC SS
 python 4_train_GP_model.py --load Nx --BC CL
 python 4_train_GP_model.py --load Nxy --BC SS
 python 4_train_GP_model.py --load Nxy --BC CL
