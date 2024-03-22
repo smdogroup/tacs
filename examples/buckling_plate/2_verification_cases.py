a5c98ce5f8234b05da0f4bdc784b82f024f1cfcd
seanfireball1 <sean.engelstad1@gmail.com>
2024-03-22 15:37:07 -0400

    updated verification cases

--- examples/buckling_plate/2_verification_cases.py
+++ examples/buckling_plate/2_verification_cases.py
@@ -43,7 +43,7 @@ class TestPlateCases(unittest.TestCase):
 
         # flat_plate.run_static_analysis(write_soln=True)
 
-        tacs_eigvals = self.flat_plate.run_buckling_analysis(
+        tacs_eigvals,_ = self.flat_plate.run_buckling_analysis(
             sigma=30.0, num_eig=12, write_soln=True
         )
 
@@ -75,7 +75,7 @@ class TestPlateCases(unittest.TestCase):
 
         # flat_plate.run_static_analysis(write_soln=True)
 
-        tacs_eigvals = self.flat_plate.run_buckling_analysis(
+        tacs_eigvals,_ = self.flat_plate.run_buckling_analysis(
             sigma=30.0, num_eig=12, write_soln=True
         )
 
@@ -110,7 +110,7 @@ class TestPlateCases(unittest.TestCase):
 
         # flat_plate.run_static_analysis(write_soln=True)
 
-        tacs_eigvals = self.flat_plate.run_buckling_analysis(
+        tacs_eigvals,_ = self.flat_plate.run_buckling_analysis(
             sigma=30.0, num_eig=12, write_soln=True
         )
 
