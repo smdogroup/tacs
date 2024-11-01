ad1ec1a02c2ec76911950d38a7adc9ed6f9d59b2
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2024-11-01 13:09:03 -0400

    Making mpi4py 3.1.5 new minimum version for build

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     python_requires=">=3.9.0",
     install_requires=[
         "numpy<2.0.0",
-        "mpi4py>=3.1.1,<4.0.0",
+        "mpi4py>=3.1.5,<4.0.0",
         "scipy>=1.2.1",
         "pynastran>=1.4.0",
         "numba",
