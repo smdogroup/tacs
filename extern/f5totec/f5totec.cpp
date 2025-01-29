020e84aad758b93843c1fbe523f28675abe08a75
Alasdair Gray <alachris@umich.edu>
2025-01-28 19:46:20 -0500

    Working implementation writing out discontinuous data (I think)

--- extern/f5totec/f5totec.cpp
+++ extern/f5totec/f5totec.cpp
@@ -42,6 +42,25 @@ enum ZoneType {
   FEPOLYHEDRA
 };
 
+/**
+ * @brief Convert from TACS element node ordering to tecplot node ordering
+ *
+ * @param eltype Element type
+ * @param ind TACS node number
+ * @return int Tecplot node number
+ */
+int convertLocalNodeInd(const ElementLayout eltype, const int ind) {
+  if (eltype == TACS_QUAD_ELEMENT) {
+    const int convert[] = {0, 1, 3, 2};
+    return convert[ind];
+  } else if (eltype == TACS_HEXA_ELEMENT) {
+    const int convert[] = {0, 1, 3, 2, 4, 5, 7, 6};
+    return convert[ind];
+  } else {
+    return ind;
+  }
+}
+
 /*
   Initialize a data file.
 
@@ -202,21 +221,21 @@ int main(int argc, char *argv[]) {
     }
 
     int num_elements;
-    int *element_comp_num, *ltypes, *ptr, *conn;
-    loader->getConnectivity(&num_elements, &element_comp_num, &ltypes, &ptr,
+    int *element_comp_num, *eltypes, *ptr, *conn;
+    loader->getConnectivity(&num_elements, &element_comp_num, &eltypes, &ptr,
                             &conn);
 
     const char *cname, *var_names;
-    int num_points, num_variables;
+    int num_points, num_cvars_per_node;
     float *cdata;
-    loader->getContinuousData(&cname, &var_names, &num_points, &num_variables,
-                              &cdata);
+    loader->getContinuousData(&cname, &var_names, &num_points,
+                              &num_cvars_per_node, &cdata);
 
     const char *ename, *evar_names;
-    int edim1, num_evariables;
+    int num_enodes, num_evars_per_node;
     float *edata;
-    loader->getElementData(&ename, &evar_names, &edim1, &num_evariables,
-                           &edata);
+    loader->getElementData(&ename, &evar_names, &num_enodes,
+                           &num_evars_per_node, &edata);
 
     double solution_time = 0.0;
 
@@ -243,14 +262,13 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    // For each component, average the nodal data
-    float *avg_edata = new float[num_points * num_evariables];
     // Nodally average the data
-    memset(avg_edata, 0, num_points * num_evariables * sizeof(float));
-    for (int j = 0; j < num_evariables; j++) {
+    float *avg_edata = new float[num_points * num_evars_per_node];
+    memset(avg_edata, 0, num_points * num_evars_per_node * sizeof(float));
+    for (int j = 0; j < num_evars_per_node; j++) {
       for (int k = 0; k < ptr[num_elements]; k++) {
-        avg_edata[num_evariables * conn[k] + j] +=
-            counts[conn[k]] * edata[num_evariables * k + j];
+        avg_edata[num_evars_per_node * conn[k] + j] +=
+            counts[conn[k]] * edata[num_evars_per_node * k + j];
       }
     }
 
@@ -266,72 +284,110 @@ int main(int argc, char *argv[]) {
     int num_basic_elements = 0;
     int basic_conn_size = 0;
     for (int k = 0; k < num_elements; k++) {
-      int ntypes = 0, nconn = 0;
-      ElementLayout ltype = (ElementLayout)ltypes[k];
-      TacsConvertVisLayoutToBasicCount(ltype, &ntypes, &nconn);
+      int numNewElems = 0, numNewNodes = 0;
+      const ElementLayout eltype = (ElementLayout)eltypes[k];
+      TacsConvertVisLayoutToBasicCount(eltype, &numNewElems, &numNewNodes);
       // For triangular elements we'll add a 4th dummy node
       // to the connectivity that's just the third node repeated
       // This way triangles can be treated as degenerate quads from Tecplot's
       // perspective
-      if (ltype == TACS_TRI_ELEMENT || ltype == TACS_TRI_QUADRATIC_ELEMENT ||
-          ltype == TACS_TRI_CUBIC_ELEMENT) {
-        nconn = nconn + ntypes;
+      if (eltype == TACS_TRI_ELEMENT || eltype == TACS_TRI_QUADRATIC_ELEMENT ||
+          eltype == TACS_TRI_CUBIC_ELEMENT) {
+        numNewNodes = numNewNodes + numNewElems;
       }
       // Plot higher order tetrahedral elements as a single linear element
-      else if (ltype == TACS_TETRA_QUADRATIC_ELEMENT ||
-               ltype == TACS_TETRA_CUBIC_ELEMENT) {
-        nconn = 4;
-        ntypes = 1;
+      else if (eltype == TACS_TETRA_QUADRATIC_ELEMENT ||
+               eltype == TACS_TETRA_CUBIC_ELEMENT) {
+        numNewNodes = 4;
+        numNewElems = 1;
       }
-      num_basic_elements += ntypes;
-      basic_conn_size += nconn;
+      num_basic_elements += numNewElems;
+      basic_conn_size += numNewNodes;
     }
 
-    int *basic_ltypes = new int[num_basic_elements];
+    int *const basic_element_data_map =
+        new int[num_basic_elements];  // For each basic element, where does the
+                                      // data for the corresponding original
+                                      // element start in the element data array
+    int *elem_data_map_ptr = basic_element_data_map;
+
+    int *basic_eltypes = new int[num_basic_elements];
     int *basic_element_comp_num = new int[num_basic_elements];
     int *basic_conn = new int[basic_conn_size];
+    int *const basic_node_map =
+        new int[basic_conn_size];  // Which node in the original element is this
+                                   // node in the basic element
+    int *basic_node_map_ptr = basic_node_map;
 
-    int *btypes = basic_ltypes;
+    int *btypes = basic_eltypes;
     int *belem_comp_num = basic_element_comp_num;
     int *bconn = basic_conn;
+
+    int origNodeCount = 0;
+
     for (int k = 0; k < num_elements; k++) {
-      int ntypes = 0, nconn = 0;
-      ElementLayout ltype = (ElementLayout)ltypes[k];
+      int numNewElems = 0, numNewNodes = 0;
+      ElementLayout eltype = (ElementLayout)eltypes[k];
+      const int nodes_per_elem = TacsGetNumVisNodes(eltype);
       // Add our dummy nodes for triangular elements
-      if (ltype == TACS_TRI_ELEMENT || ltype == TACS_TRI_QUADRATIC_ELEMENT ||
-          ltype == TACS_TRI_CUBIC_ELEMENT) {
-        TacsConvertVisLayoutToBasicCount(ltype, &ntypes, &nconn);
-        int *tri_conn = new int[nconn];
-        TacsConvertVisLayoutToBasic(ltype, &conn[ptr[k]], btypes, tri_conn);
+      if (eltype == TACS_TRI_ELEMENT || eltype == TACS_TRI_QUADRATIC_ELEMENT ||
+          eltype == TACS_TRI_CUBIC_ELEMENT) {
+        TacsConvertVisLayoutToBasicCount(eltype, &numNewElems, &numNewNodes);
+        int *tri_conn = new int[numNewNodes];
+        TacsConvertVisLayoutToBasic(eltype, &conn[ptr[k]], btypes, tri_conn);
 
         const int convert[] = {0, 1, 2, 2};
-        for (int jj = 0; jj < ntypes; jj++) {
+        for (int jj = 0; jj < numNewElems; jj++) {
           for (int ii = 0; ii < 4; ii++) {
             bconn[4 * jj + ii] = tri_conn[3 * jj + convert[ii]];
           }
           btypes[jj] = TACS_QUAD_ELEMENT;
         }
-        nconn = nconn + ntypes;
+        numNewNodes = numNewNodes + numNewElems;
         delete[] tri_conn;
       }
       // Plot only the first four nodes (conrners) of higher order tets
-      else if (ltype == TACS_TETRA_QUADRATIC_ELEMENT ||
-               ltype == TACS_TETRA_CUBIC_ELEMENT) {
+      else if (eltype == TACS_TETRA_QUADRATIC_ELEMENT ||
+               eltype == TACS_TETRA_CUBIC_ELEMENT) {
         memcpy(bconn, &conn[ptr[k]], 4 * sizeof(int));
-        nconn = 4;
-        ntypes = 1;
+        numNewNodes = 4;
+        numNewElems = 1;
         btypes[0] = TACS_TETRA_ELEMENT;
       } else {
-        TacsConvertVisLayoutToBasicCount(ltype, &ntypes, &nconn);
-        TacsConvertVisLayoutToBasic(ltype, &conn[ptr[k]], btypes, bconn);
+        TacsConvertVisLayoutToBasicCount(eltype, &numNewElems, &numNewNodes);
+        TacsConvertVisLayoutToBasic(eltype, &conn[ptr[k]], btypes, bconn);
       }
       // Set the basic element component to match the parent
-      for (int ii = 0; ii < ntypes; ii++) {
+      for (int ii = 0; ii < numNewElems; ii++) {
         belem_comp_num[ii] = element_comp_num[k];
       }
-      btypes += ntypes;
-      belem_comp_num += ntypes;
-      bconn += nconn;
+      // Copy data to the basic element data
+      for (int ii = 0; ii < numNewNodes; ii++) {
+        // Find which node this is in the original element
+        int elemLocalInd = -1;
+        for (int jj = 0; jj < nodes_per_elem; jj++) {
+          if (conn[ptr[k] + jj] == bconn[ii]) {
+            elemLocalInd = jj;
+            break;
+          }
+        }
+        if (elemLocalInd == -1) {
+          fprintf(stderr, "Error, node not found in original element\n");
+          return 1;
+        }
+        basic_node_map_ptr[ii] = elemLocalInd;
+      }
+
+      for (int jj = 0; jj < numNewElems; jj++) {
+        elem_data_map_ptr[jj] = origNodeCount * num_evars_per_node;
+      }
+      origNodeCount += nodes_per_elem;
+      elem_data_map_ptr += numNewElems;
+
+      btypes += numNewElems;
+      belem_comp_num += numNewElems;
+      bconn += numNewNodes;
+      basic_node_map_ptr += numNewNodes;
     }
 
     int num_comp = loader->getNumComponents();
@@ -340,6 +396,14 @@ int main(int argc, char *argv[]) {
     int *reduced_conn = new int[basic_conn_size];
     float *reduced_float_data = NULL;
     reduced_float_data = new float[num_points];
+    int *const compNodeGlobalInd =
+        new int[basic_conn_size];  // Map from the duplicated node index within
+                                   // a component to the index of that node in
+                                   // the original full mesh
+    int *const compBasicElemInd =
+        new int[num_basic_elements];  // Map from the index of a basic element
+                                      // within a component to the index of that
+                                      // basic element in the full mesh
 
     for (int k = 0; k < num_comp; k++) {
       // Count up the number of elements that use the connectivity
@@ -348,37 +412,33 @@ int main(int argc, char *argv[]) {
       //  k, comp_name, solution_time);
 
       memset(reduced_points, 0, num_points * sizeof(int));
+      memset(compNodeGlobalInd, 0, basic_conn_size * sizeof(int));
       memset(reduced_conn, 0, basic_conn_size * sizeof(int));
+      memset(compBasicElemInd, 0, num_basic_elements * sizeof(int));
 
-      int npts = 1, nelems = 0;
+      int npts = 1, num_elements = 0;
       int zone_btype = -1;
       int basic_conn_offset = 0;
       // Count up the number of points/elements in this sub-domain
       for (int i = 0; i < num_basic_elements; i++) {
-        ElementLayout ltype = (ElementLayout)basic_ltypes[i];
-        int conn_size = TacsGetNumVisNodes(ltype);
+        ElementLayout eltype = (ElementLayout)basic_eltypes[i];
+        int num_node_per_elem = TacsGetNumVisNodes(eltype);
 
         if (basic_element_comp_num[i] == k) {
           // Make sure all elements in this zone are the same type
           if (zone_btype == -1) {
-            zone_btype = basic_ltypes[i];
-          } else if (zone_btype != basic_ltypes[i]) {
+            zone_btype = basic_eltypes[i];
+          } else if (zone_btype != basic_eltypes[i]) {
             fprintf(stderr, "Component %d has conflicting element types\n", k);
             return (1);
           }
+          compBasicElemInd[num_elements] = i;
 
           int pt;
-          for (int j = 0; j < conn_size; j++) {
+          for (int j = 0; j < num_node_per_elem; j++) {
             // Add this element to the reduced connectivity
-            if (basic_ltypes[k] == TACS_QUAD_ELEMENT) {
-              const int convert[] = {0, 1, 3, 2};
-              pt = basic_conn[basic_conn_offset + convert[j]];
-            } else if (basic_ltypes[k] == TACS_HEXA_ELEMENT) {
-              const int convert[] = {0, 1, 3, 2, 4, 5, 7, 6};
-              pt = basic_conn[basic_conn_offset + convert[j]];
-            } else {
-              pt = basic_conn[basic_conn_offset + j];
-            }
+            pt = basic_conn[basic_conn_offset + convertLocalNodeInd(eltype, j)];
+            compNodeGlobalInd[num_elements * num_node_per_elem + j] = pt;
 
             // If a reduced numbering has not been applied to this point,
             // create a new number for it
@@ -388,12 +448,13 @@ int main(int argc, char *argv[]) {
             }
 
             // Set the reduced connectivity
-            reduced_conn[conn_size * nelems + j] = reduced_points[pt];
+            reduced_conn[num_node_per_elem * num_elements + j] =
+                reduced_points[pt];
           }
 
-          nelems++;
+          num_elements++;
         }
-        basic_conn_offset += conn_size;
+        basic_conn_offset += num_node_per_elem;
       }
 
       // Since we started at npts = 1, we have one more point
@@ -416,35 +477,119 @@ int main(int argc, char *argv[]) {
         return (1);
       }
 
-      if (nelems > 0 && npts > 0) {
-        // Create the zone with the solution time
-        create_fe_tec_zone(comp_name, zone_type, npts, nelems, use_strands,
-                           solution_time);
+      if (num_elements > 0 && npts > 0) {
+        const bool duplicateNodes = true;
+        if (duplicateNodes) {
+          // In this case, we duplicate nodes so that every element has its own
+          // nodes (e.g first element has nodes 0->n, second elements has nodes
+          // n+1->2n etc) and we can plot discontinuous data
 
-        // Retrieve the continuous data
-        for (int j = 0; j < num_variables; j++) {
-          for (int i = 0; i < num_points; i++) {
-            if (reduced_points[i] > 0) {
-              reduced_float_data[reduced_points[i] - 1] =
-                  cdata[i * num_variables + j];
+          // Create the zone with the solution time
+          const int nodes_per_elem =
+              TacsGetNumVisNodes((ElementLayout)zone_btype);
+          const int num_dup_nodes = num_elements * nodes_per_elem;
+          create_fe_tec_zone(comp_name, zone_type, num_dup_nodes, num_elements,
+                             use_strands, solution_time);
+
+          // Allocate a new array for the nodal data
+          float *const nodalData = new float[num_dup_nodes];
+
+          // Continuous data
+          for (int jj = 0; jj < num_cvars_per_node; jj++) {
+            for (int elemInd = 0; elemInd < num_elements; elemInd++) {
+              for (int nodeInd = 0; nodeInd < nodes_per_elem; nodeInd++) {
+                const int dupNodeInd = elemInd * nodes_per_elem + nodeInd;
+                const int globalNodeInd = compNodeGlobalInd[dupNodeInd];
+                nodalData[dupNodeInd] =
+                    cdata[globalNodeInd * num_cvars_per_node + jj];
+              }
             }
+            write_tec_float_data(num_dup_nodes, nodalData);
           }
-          write_tec_float_data(npts, reduced_float_data);
-        }
+          // Element data
+          // The TACS data is stored such that all variable values for a given
+          // node are contiguous, tecplot wants the data to be stored such that
+          // all values for a given variable are contiguous
+          for (int jj = 0; jj < num_evars_per_node; jj++) {
+            for (int elemInd = 0; elemInd < num_elements; elemInd++) {
+              const int globalElemInd =
+                  compBasicElemInd[elemInd];  // Global basic element index
+              const int elemDataStartInd =
+                  basic_element_data_map[globalElemInd];  // Start of data for
+                                                          // this element in the
+                                                          // element data array
+              const float *const origElementData =
+                  &edata[elemDataStartInd];  // Start of data for this element
 
-        // Retrieve the element data
-        for (int j = 0; j < num_evariables; j++) {
-          for (int i = 0; i < num_points; i++) {
-            if (reduced_points[i] > 0) {
-              reduced_float_data[reduced_points[i] - 1] =
-                  avg_edata[num_evariables * i + j];
+              for (int nodeInd = 0; nodeInd < nodes_per_elem; nodeInd++) {
+                // At this point I know:
+                // - Which basic element I'm handling
+                // - Which original element that basic element came from
+                // - Where in the element data array does the data for this
+                // original element start
+                // - Which node (in TACS ordering) within the basic element I'm
+                // handling
+                // - Which node of the basic element this in the tecplot
+                // ordering
+                // - Which node in the original element is this
+
+                const int dupNodeInd = elemInd * nodes_per_elem + nodeInd;
+
+                const int basicElemLocalNodeInd =
+                    convertLocalNodeInd((ElementLayout)zone_btype, nodeInd);
+
+                const int originalElemLocalNodeInd =
+                    basic_node_map[globalElemInd * nodes_per_elem +
+                                   basicElemLocalNodeInd];
+
+                nodalData[dupNodeInd] =
+                    origElementData[originalElemLocalNodeInd *
+                                        num_evars_per_node +
+                                    jj];
+              }
             }
+            write_tec_float_data(num_dup_nodes, nodalData);
           }
-          write_tec_float_data(npts, reduced_float_data);
-        }
 
-        // Now, write the connectivity
-        write_con_data(reduced_conn);
+          // Now create the connectivity, because of the way we duplicate the
+          // nodal values, the connectivity is just 1,2,3,4...
+          int *const dupConn = new int[num_dup_nodes];
+          for (int i = 0; i < num_dup_nodes; i++) {
+            dupConn[i] = i + 1;
+          }
+          write_con_data(dupConn);
+          delete[] nodalData;
+          delete[] dupConn;
+        } else {
+          // Create the zone with the solution time
+          create_fe_tec_zone(comp_name, zone_type, npts, num_elements,
+                             use_strands, solution_time);
+
+          // Retrieve the continuous data
+          for (int j = 0; j < num_cvars_per_node; j++) {
+            for (int i = 0; i < num_points; i++) {
+              if (reduced_points[i] > 0) {
+                reduced_float_data[reduced_points[i] - 1] =
+                    cdata[i * num_cvars_per_node + j];
+              }
+            }
+            write_tec_float_data(npts, reduced_float_data);
+          }
+
+          // Retrieve the element data
+          for (int j = 0; j < num_evars_per_node; j++) {
+            for (int i = 0; i < num_points; i++) {
+              if (reduced_points[i] > 0) {
+                reduced_float_data[reduced_points[i] - 1] =
+                    avg_edata[num_evars_per_node * i + j];
+              }
+            }
+            write_tec_float_data(npts, reduced_float_data);
+          }
+
+          // Now, write the connectivity
+          write_con_data(reduced_conn);
+        }
       }
     }
 
@@ -460,9 +605,13 @@ int main(int argc, char *argv[]) {
     delete[] reduced_float_data;
     delete[] avg_edata;
 
-    delete[] basic_ltypes;
+    delete[] basic_eltypes;
     delete[] basic_conn;
     delete[] basic_element_comp_num;
+    delete[] basic_node_map;
+    delete[] basic_element_data_map;
+    delete[] compNodeGlobalInd;
+    delete[] compBasicElemInd;
 
     delete[] infile;
     delete[] outfile;
