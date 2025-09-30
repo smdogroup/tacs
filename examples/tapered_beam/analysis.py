dcc354c67b1cb14560af24a50c77df2de10a5159
Alasdair Gray <alachris@umich.edu>
2025-09-29 21:59:43 -0400

    Tapered beam example

---
+++ examples/tapered_beam/analysis.py
@@ -0,0 +1,37 @@
+"""
+==============================================================================
+Modal analysis of a tapered beam using TACS
+==============================================================================
+@File    :   analysis.py
+@Date    :   2025/09/29
+@Author  :   Alasdair Christison Gray
+@Description :
+"""
+
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+import argparse
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+from tacs import pyTACS
+
+parser = argparse.ArgumentParser(description="Tapered Beam Analysis")
+parser.add_argument(
+    "-i", "--input", type=str, default="tapered_beam_pbeam.bdf", help="Input BDF file"
+)
+args = parser.parse_args()
+
+FEASolver = pyTACS(args.input)
+FEASolver.initialize()
+problems = FEASolver.createTACSProbsFromBDF()
+for problem in problems.values():
+    problem.setOption("printLevel", 2)
+    problem.solve()
+    problem.writeSolution()
