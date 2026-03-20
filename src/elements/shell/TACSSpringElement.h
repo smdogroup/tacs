450706f60d58b704f52a3a46b3ebd64af8bab009
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2026-03-20 13:52:29 +0000

    Adding layut tpe to spring element

--- src/elements/shell/TACSSpringElement.h
+++ src/elements/shell/TACSSpringElement.h
@@ -21,6 +21,7 @@
 #include "TACSElement.h"
 #include "TACSGeneralSpringConstitutive.h"
 #include "TACSSpringElementTransform.h"
+#include "TACSElementTypes.h"
 
 class TACSSpringElement : public TACSElement {
  public:
@@ -107,6 +108,8 @@ class TACSSpringElement : public TACSElement {
                                const TacsScalar dfdq[], TacsScalar dfdXpts[]) {
     return;
   }
+
+  ElementLayout getLayoutType() { return TACS_LINE_ELEMENT; }
 
  private:
   void transformVarsGlobalToLocal(const TacsScalar t[],
