b5f6da84875f844e025bb1084729ac1b34c585d9
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2026-05-29 13:25:43 -0400

    Bump minor version and remove stale deprecations (#451)
    * Remove deprecated methods and options from TACSShellElement and StaticProblem classes
    
    * Update deprecation warning for LamParamShellConstitutive to specify removal in version 3.14.0
    
    * Bump version to 3.12.0 in setup.py

--- tacs/constitutive.pyx
+++ tacs/constitutive.pyx
@@ -2382,7 +2382,7 @@ cdef class SmearedCompositeShellConstitutive(ShellConstitutive):
 
 def LamParamShellConstitutive(*args, **kwargs):
     warnings.warn(
-        "LamParamShellConstitutive is deprecated and will be removed in a future version. "
+        "LamParamShellConstitutive is deprecated and will be removed in version 3.14.0. "
         "Use LamParamSmearedShellConstitutive instead.",
         DeprecationWarning,
         stacklevel=2,
