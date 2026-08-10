db0814b87a67eac9123cb29f634fdfbf2cdce9ca
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2026-08-10 11:21:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ setup(
         "mpi4py>=4.0.3,<5.0.0",
         "scipy>=1.2.1",
         # pin pynastran against last commit that supports python 3.10
-        "pynastran @ git+https://github.com/smdogroup/pyNastran.git@e76ee3bebcf44abff38a6475f34bf61d750789d6",
+        "pynastran @ git+https://github.com/smdogroup/pyNastran.git@60b740fc5a79ff968afa48a6dfbc3f882187d820",
         "numba",
         "packaging",
     ],
