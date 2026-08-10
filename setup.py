c52c963669029fd3eb681f0533990318cc5a3a33
timryanb <41971846+timryanb@users.noreply.github.com>
2026-08-10 16:25:18 +0000

    version bump to 3.12.3

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ optional_dependencies["all"] = sorted(
 
 setup(
     name="tacs",
-    version="3.12.2",
+    version="3.12.3",
     description="Parallel finite-element analysis package",
     long_description=long_description,
     long_description_content_type="text/markdown",
